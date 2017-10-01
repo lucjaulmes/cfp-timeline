a06acf16e5a1347d8a9e8f2c5d93a78ce99a9a53
Luc Jaulmes <luc.jaulmes@bsc.es>
2017-10-01 21:52:35 +0200

    A week-end's hacking

---
+++ core-cfp.js
@@ -0,0 +1,257 @@
+var datatable, today = new Date();
+var timeline_zero = Date.UTC(today.getFullYear(), today.getMonth() - 6);
+// px per month
+var timeline_scale = 50 / Date.UTC(1970, 1, 1)
+
+var ranks = ['A*', 'A', 'B', 'C', 'D'];
+function ranksort(a, b) { return ranks.indexOf(a) - ranks.indexOf(b); }
+
+/* https://stackoverflow.com/a/19015262 */
+function getScrollBarWidth() {
+    var outer = $('<div>').css({visibility: 'hidden', width: 100, overflow: 'scroll'}).appendTo('body'),
+        widthWithScroll = $('<div>').css({width: '100%'}).appendTo(outer).outerWidth();
+    outer.remove();
+    return 100 - widthWithScroll;
+};
+
+var offset = getScrollBarWidth();
+
+function resizeHeaderBackground() {
+	var timeline = $('#timeline'), tl_dom = timeline.get(0);
+	var bar_v = tl_dom.scrollWidth > timeline.innerWidth() | 0;
+	var bar_h = tl_dom.scrollHeight > timeline.innerHeight() | 0;
+
+	$('#header').width(timeline.outerWidth(true) - offset * bar_h).height(timeline.outerHeight(true) - offset * bar_v);
+}
+
+function makeTimelineLegend(maxdate) {
+	var box = $('#header')
+	var startDate = new Date(timeline_zero);
+	var endDate = typeof maxdate !== 'undefined' ? new Date(maxdate) :
+		new Date(Date.UTC(today.getFullYear(), today.getMonth() + 12));
+
+	// get last day of month
+	// go to first day to avoid side effects of incrementing month
+	endDate.setDate(1);
+
+	// then increment month and go to day -1
+	endDate.setMonth(endDate.getMonth() + 36);
+	endDate.setDate(0);
+
+	box.empty()
+
+	var pm = $('<p id="months"></p>').prependTo(box), m, last = timeline_zero;
+	var month_name = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+	$('<span class="acronyms">&nbsp;</span>').appendTo(pm);
+	for (m = today.getMonth() - 6; last < endDate; m++)
+	{
+		var next = Date.UTC(today.getFullYear(), m + 1), month = new Date(last).getMonth();
+		$('<span></span>').append(month_name[month])
+			.addClass((month == 11 ? 'december' : ''))
+			.css('width', (next - last) * timeline_scale - (month == 11 ? 2 : 1)).appendTo(pm);
+		last = next;
+	}
+
+	var py = $('<p id="years"></p>').prependTo(box);
+	$('<span class="acronyms">&nbsp;</span>').appendTo(py);
+	for (var y = startDate.getFullYear(); y <= endDate.getFullYear(); y++)
+	{
+		var from = Math.max(timeline_zero, Date.UTC(y, 0));
+		var to = Math.min(last, Date.UTC(y, 12));
+		$('<span></span>').append(y).css('width', (to - from) * timeline_scale - (to < last ? 2 : 1)).appendTo(py);
+	}
+
+	resizeHeaderBackground();
+	setInterval(resizeHeaderBackground, 100);
+
+	$('#timeline').on('scroll', function() {
+		$('#months').scrollLeft($(this).scrollLeft());
+		$('#years').scrollLeft($(this).scrollLeft());
+	});
+}
+
+function parse_date(str) {
+	if (!str) return null;
+	tok = str.split('-');
+	return Math.max(timeline_zero, Date.UTC(tok[0], tok[1], tok[2]));
+}
+
+function addToTimeline(n, data) {
+	var s = timeline_scale, last = timeline_zero, tooltip;
+	var p = $('<p></p>').append($('<span class="acronyms"></span>').append(data[0]));
+
+	var abst = parse_date(data[3]);
+	var submit = parse_date(data[4]);
+	var notif = parse_date(data[5]);
+
+	if (!abst) abst = submit;
+	else if (!submit) submit = abst;
+
+	if (submit && notif) {
+		if (submit > abst) {
+			tooltip = $('<span></span>').addClass('tooltip').append(data[0]+' registration '+data[3]);
+			$('<span class="abstract"></span>')
+				.css('margin-left', (abst - last) * s)
+				.css('padding-right', (submit - abst) * s)
+				.css('width', 0)
+				.html(data[11] === false ? '<sup>†</sup>' : '').append(tooltip).appendTo(p);
+
+			last = submit
+		}
+
+		tooltip = $('<span></span>').addClass('tooltip').append(data[0]+' submission '+data[4])
+									.append(',<br />notification '+data[5]);
+		$('<span class="review"></span>')
+			.css('margin-left', (submit - last) * s)
+			.css('padding-right', (notif - submit) * s)
+			.css('width', 0)
+			.html(data[12] === false ? '<sup>†</sup>' : '').append(tooltip).appendTo(p);
+
+		if (data[13] === false)
+			$('<span><sup>†</sup></span>').css('width', 0).appendTo(p);
+		last = notif;
+
+	} else if (submit) {
+		tooltip = $('<span></span>').addClass('tooltip').append(data[0]+' submission '+data[4]);
+
+		$('<span class="submit"><sup>◆</sup></span>')
+			.css('margin-left', (submit - last) * s).css('width', 0)
+			.append(data[12] === false ? '<sup>†</sup>' : '').append(tooltip).appendTo(p);
+		last = submit
+	}
+
+	var camera = parse_date(data[6]);
+	if (camera) {
+		tooltip = $('<span></span>').addClass('tooltip').append(data[0]+' final version '+data[6]);
+		$('<span class="camera"><sup>∎</sup></span>')
+			.css('margin-left', (camera - last) * s).css('width', 0)
+			.append(data[14] === false ? '<sup>†</sup>' : '').append(tooltip).appendTo(p);
+		last = camera
+	}
+
+	var start = parse_date(data[7]);
+	var end = parse_date(data[8]);
+	if (start && end) {
+		tooltip = $('<span></span>').addClass('tooltip').append(data[0]+' from '+data[7]+' to '+data[8]);
+
+		$('<span class="conf"></span>')
+			.css('margin-left', (start - last) * s)
+			.css('padding-right', (end - start) * s)
+			.css('width', 0)
+			.append(data[15] === false ? '<sup>†</sup>' : '').append(tooltip).appendTo(p);
+
+		if (data[16] === false)
+			$('<span><sup>†</sup></span>').css('width', 0).appendTo(p);
+
+		last = end;
+	}
+
+	// ignore conferences for which we don't have a single date
+	if (last > timeline_zero) {
+		$('#timeline').append(p)
+	}
+}
+
+function filterUpdated(search) {
+	var filteredData = datatable.rows({filter:'applied'}).data();
+	$('#timeline').empty()
+	$.each(filteredData, addToTimeline)
+}
+
+function makeFilter(column, sortfunction) {
+	var values = column.data().unique().sort(sortfunction);
+
+	var select = $('<select multiple></select>')
+		.attr('size', Math.min(10, values.length))
+		.on('change', function () {
+			var val = $.map($(this).val(), $.fn.dataTable.util.escapeRegex), regex = '';
+			if (val.length)
+				regex = '^('+val.join('|')+')$';
+			column.search(regex, true, false).draw();
+		});
+
+	values.each(function (t) {
+		select.append('<option value="'+t+'">'+t+'</option>');
+	});
+
+	$('<p></p>').appendTo( $("#filters") ).append(column.header().innerHTML).append(select).append(
+		$('<button>clear</button>').click(function(){ select.val([]).change() })
+	);
+}
+
+function renderAcronym(data, type, row) {
+	if (type === 'display' && row[10] && row[10] != '(missing)')
+		return $('<a></a>').attr('href', row[10]).append(data)[0].outerHTML;
+	else
+		return data;
+}
+
+function markExtrapolated(td, data, rowdata, row, col) {
+	if (data && rowdata[col+8] === false)
+		$(td).addClass('extrapolated');
+}
+
+function notNull(val, idx) {
+	return val != null
+}
+
+function populatePage(json) {
+	datatable = $('#confs').DataTable({
+		data: json['data'],
+		columns: json['columns'],
+		columnDefs: [
+			// links, searchable but not displayed
+			{"targets": [10], "visible": false, "searchable": true},
+			// acronyms, displayed wrapped in links if present
+			{"targets": [0], "render": renderAcronym},
+			// dates, display whether extrapolated
+			{"targets": [3, 4, 5, 6, 7, 8], "createdCell": markExtrapolated},
+			// booleans on extrapolated dates, hiddent
+			{"targets": [11, 12, 13, 14, 15, 16], "visible": false, "searchable": false}
+		],
+		pageLength: 50,
+		order: [[4, "asc"], [3, "asc"], [7, "asc"], [8, "asc"]]
+	});
+
+	show = $('<button>&gt;</button>')
+	filter = $('<div id="filters"></div>')
+
+	show.click(function() {
+		filter.slideToggle();
+		show.toggleClass('rotated');
+	});
+	show.wrap('<label id="show_filters">Filters</label>');
+	$('#confs_filter').append(filter).prepend(show.parent())
+	makeFilter(datatable.column(0));
+	makeFilter(datatable.column(2), ranksort);
+	makeFilter(datatable.column(9));
+
+	datatable.draw().on('search.dt', filterUpdated);
+
+	$.each(datatable.rows().data(), addToTimeline)
+
+	var maxdate = timeline_zero;
+	for (var col = 3; col <= 8; col++)
+	{
+		var colmax = datatable.column(col).data().filter(notNull).sort().reverse()[0];
+		// lexical sort works thanks to format, just parse once
+		maxdate = Math.max(maxdate, parse_date(colmax));
+	}
+
+	makeTimelineLegend(maxdate);
+}
+
+$(document).ready(function() {
+	makeTimelineLegend()
+
+	/* $.getJSON + override mime type */
+	$.ajax({
+		url: "cfp.json",
+		dataType: "json",
+		beforeSend: function( xhr ) {
+			xhr.overrideMimeType('application/json; charset="UTF-8"');
+		},
+		success: populatePage
+	});
+});
+
---
+++ fetch_core+cfp.py
@@ -0,0 +1,250 @@
+#!/usr/bin/python3
+
+import sys
+import json
+import requests
+from datetime import datetime, date
+from urllib.parse import urlparse
+from bs4 import BeautifulSoup
+
+
+def head(n, iterator):
+	""" Return the first (up to) n elements of an iterator
+	"""
+	for _ in range(n):
+		yield next(iterator)
+
+
+def uniq(it, **sorted_kwargs):
+	""" Sort the iterator using sorted(it, **sorted_kwargs) and return
+		all non-duplicated elements (for !=)
+	"""
+	y = next(it)
+	yield y
+	for x in sorted(it, **sorted_kwargs):
+		if x != y:
+			yield x
+			y = x
+
+
+def get_soup(url, filename, **kwargs):
+	""" Simple caching mechanism. Fetch a page from url and save it in filename.
+
+		If filename exists, return its contents instead.
+	"""
+	try:
+		with open(filename, 'r') as fh:
+			soup = BeautifulSoup(fh.read(), 'lxml')
+	except FileNotFoundError:
+		r = requests.get(url, **kwargs)
+		with open(filename, 'w') as fh:
+			print(r.text, file=fh)
+		soup = BeautifulSoup(r.text, 'lxml')
+	return soup
+
+
+ranks = ['A*', 'A', 'B', 'C', 'D']
+def ranksort(conf): # lower is better
+	""" Utility to sort the ranks based on the order we want (specificially A* < A).
+	"""
+	try: return (ranks.index(conf['rank']), conf['acronym'].lower())
+	except ValueError: return (len(ranks), conf['acronym'].lower()) # non-ranked, e.g. 'Australasian'
+
+
+def parse_date(d):
+	return datetime.strptime(d, '%b %d, %Y').date()
+
+
+def json_encode_dates(obj):
+	if isinstance(obj, date):
+		return str(obj)
+	else:
+		raise TypeError('{} not encodable'.format(obj))
+
+
+url_corerank = 'http://portal.core.edu.au/conf-ranks/?search=&by=all&source=CORE2017&sort=arank&page={}'
+url_forcodes = 'http://www.uq.edu.au/research/research-management/era-for-codes'
+wiki_cfp = 'http://www.wikicfp.com'
+url_cfpsearch = wiki_cfp + '/cfp/servlet/tool.search'
+
+missing_values = {'N/A', 'TBD'}
+
+hardcoded = { # Manually correct some errors. TODO this is not scalable.
+	("SENSYS", 2017): ["03-04-2017", "10-04-2017", "17-07-2017", None, "05-11-2017", "08-11-2017"],
+	("ISCA",   2018): ["14-11-2017", "21-11-2017", "13-03-2018", None, "02-06-2018", "06-06-2018"]
+}
+
+def get_forcodes():
+	""" Fetch and return the mapping of For Of Research (FOR) codes to the corresponding names.
+	"""
+	forcodes = {}
+	soup = get_soup(url_forcodes, 'cache/for_codes.html')
+	for row in soup.find('table').findAll('tr'):
+		try:
+			code, field = [td.text.strip() for td in row.findAll('td')]
+			if field:
+				forcodes[code] = field.capitalize()
+		except ValueError:
+			print(row.text)
+			raise
+
+	return forcodes
+
+
+def get_confs():
+	""" Generator of all conferences listed on the core site, as dicts
+	"""
+	for p in range(32): #NB hardcoded number of pages on the core site.
+		f = 'cache/ranked_{}-{}.html'.format(50 * p + 1, 50 * (p + 1))
+		soup = get_soup(url_corerank.format(p), f)
+
+		table = soup.find('table')
+		rows = iter(table.findAll('tr'))
+
+		headers = [r.text.strip().lower() for r in next(rows).findAll('th')]
+		for row in rows:
+			yield {h:v for h, v in zip(headers, [r.text.strip() for r in row.findAll('td')])}
+
+
+def strip_trailing_paren(string):
+	""" If string ends with a parenthesized part, remove it, e.g. "foo (bar)" -> "foo"
+	"""
+	return string[:string.index(' (')] if ' (' in string and string.rstrip()[-1] == ')' else string
+
+
+def fetch_core():
+	""" Generator returning all the conferences listed on the core site, sorted and unique,
+		with field (for) translated to text, pretty dict keys, etc.
+	"""
+	field_codes = get_forcodes()
+
+	for conf in uniq((c for c in get_confs() if ranksort(c)[0] < 4), key = ranksort):
+		pretty_conf = {'Acronym': strip_trailing_paren(conf['acronym']),
+				'Title': strip_trailing_paren(conf['title']),
+				'CORE 2017 Rank': conf['rank']}
+
+		if conf['for']:
+			pretty_conf['Field'] = field_codes[conf['for']]
+
+		yield pretty_conf
+
+
+def parse_cfp_soup(soup):
+	""" Parse a page from wiki-cfp. Return all useful data about the conference.
+	"""
+	# Find the the table containing the interesting data about the conference
+	# There's always a "When" and a "Where" in the info table, even though they might be N/A
+	info_table = soup.find('th', text = 'Where')
+	while info_table.name != 'table':
+		info_table = info_table.parent
+
+	# Populate data with {left cell: right cell} for every line in the table
+	it = ((tr.find('th').text, tr.find('td').text.strip()) for tr in info_table.findAll('tr'))
+	data = {th: td for th, td in it if td not in missing_values}
+
+	# find all links next to a "Link: " text, and return both their text and href values
+	l = [t.parent.find('a', href = True) for t in soup.findAll(text = lambda t: 'Link: ' in t)]
+	links = {link.text for link in l if link} | {link['href'] for link in l if link}
+
+	if len(links) == 1:
+		data['Link'] = links.pop()
+	elif len(links) > 1:
+		raise ValueError("ERROR Multiple distinct link values: " + ', '.join(links))
+
+	return data
+
+
+def get_cfp(conf, year):
+	""" Fetch the cfp from wiki-cfp for the given conference at the given year.
+	"""
+	f='cache/cfp_{}-{}.html'.format(conf['Acronym'].replace('/', '_'), year)
+	try:
+		with open(f, 'r') as fh:
+			soup = BeautifulSoup(fh.read(), 'lxml')
+
+	except FileNotFoundError:
+		search_f='cache/search_cfp_{}-{}.html'.format(conf['Acronym'].replace('/', '_'), year)
+		search_soup = get_soup(url_cfpsearch, search_f, params = {'q': conf['Acronym'], 'y': year})
+
+		conf_page = search_soup.find('a', href = True, text = lambda t: t and t.lower() == '{} {}'.format(conf['Acronym'], year).lower())
+		if conf_page:
+			soup = get_soup(wiki_cfp + conf_page['href'], f)
+		else:
+			return {}
+
+	try:
+		return parse_cfp_soup(soup)
+	except ValueError as e:
+		print('Error on conference {}: {}'.format(conf, e), file=sys.stderr)
+		raise
+
+
+def update_confs(out):
+	""" List all conferences from CORE, fetch their CfPs and print the output data as json to out.
+	"""
+	today = datetime.now().date()
+	years = [today.year, today.year + 1]
+	date_names = ['Abstract Registration Due', 'Submission Deadline', 'Notification Due', 'Final Version Due', 'Start', 'End']
+	date_orig = ['orig-abstract', 'orig-submission', 'orig-notif', 'orig-camera', 'orig-start', 'orig-end']
+
+	columns = ['Acronym', 'Title', 'CORE 2017 Rank'] + date_names + ['Field', 'Link'] + date_orig
+
+	# Columns for which we output '(missing)' instead of None
+	mark_missing = set(columns) - set(date_names) - set(date_orig)
+
+
+	print('{"columns":', file=out);
+	json.dump([{'title': c} for c in columns], out)
+	print(',\n"data": [', file=out)
+	writing_first_conf = True
+
+	for conf in fetch_core():
+		last_year = {}
+		for y in years:
+			if (conf['Acronym'].upper(), y) in hardcoded:
+
+				data = {dn: datetime.strptime(v, '%d-%m-%Y').date() for v, dn
+							in zip(hardcoded[(conf['Acronym'].upper(), y)], date_names) if v}
+
+				data.update({orig: True for v, orig
+						in zip(hardcoded[(conf['Acronym'].upper(), y)], date_orig) if v})
+
+			else:
+				data = get_cfp(conf, y)
+
+				if 'When' in data:
+					data['Start'], data['End'] = data.pop('When').split(' - ')
+
+				for dn, orig in zip(date_names, date_orig):
+					if dn in data:
+						data[orig] = True
+						data[dn] = parse_date(data[dn])
+					else:
+						# add extrapolated date from previous year
+						if dn in last_year:
+							data[orig] = False
+							data[dn] = last_year[dn].replace(year = last_year[dn].year + 1)
+						else:
+							continue
+			if data:
+				last_year = data
+				data.update(conf)
+
+				if max(data.get(c, today) for c in date_names) > today:
+					if not writing_first_conf: print(',', file=out)
+					else: writing_first_conf = False
+
+					# filter out empty values for non-date columns
+					json.dump([data.get(col, '(missing)') if col in mark_missing else data.get(col, None)
+									for col in columns], out, default = json_encode_dates)
+
+					break
+
+	print('\n]}', file=out)
+
+
+if __name__ == '__main__':
+	with open('cfp.json', 'w') as out:
+		update_confs(out)
+
+
